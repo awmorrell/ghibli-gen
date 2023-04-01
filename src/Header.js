e7827d3d9a528cd3654ce71580e8c20d03659e87
awmorrell <amorrell424@gmail.com>
2023-04-01 13:30:29 -0700

    viewport pwa

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
 		<article className="baskerville">
 		  <header id='banner' className="avenir tc ph7 pt1-ns">
 		  	<hr className="mb1"/>
-		    <h1 className="f2 fw3 mb0 dib">The Complete Works of <img class="w-30 dib mb0" src="https://upload.wikimedia.org/wikipedia/sco/c/ca/Studio_Ghibli_logo.svg" alt="ghibli logo"/></h1>
+		    <h1 className="f2 fw3 tc mb0 dib">The Complete Works of <img class="w-30 dib mb0" src="https://upload.wikimedia.org/wikipedia/sco/c/ca/Studio_Ghibli_logo.svg" alt="ghibli logo"/></h1>
 		    <hr className="mb1"/>
 		  </header>
 		</article>
